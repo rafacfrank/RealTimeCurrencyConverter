9f775fe8974ed4f0959387c7fd63622b735ae27c
Rafael Frank <rafacfrank@gmail.com>
2024-05-10 21:14:17 -0300

    Tratando exceções

--- src/br/com/rafacfrank/realtimecurrencyconverter/Main.java
+++ src/br/com/rafacfrank/realtimecurrencyconverter/Main.java
@@ -3,6 +3,7 @@ package br.com.rafacfrank.realtimecurrencyconverter;
 import br.com.rafacfrank.realtimecurrencyconverter.models.Conversion;
 import br.com.rafacfrank.realtimecurrencyconverter.models.CurrencyRequest;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -24,11 +25,15 @@ public class Main {
                 GBP - Libra Esterlina     JPY - Iene Japonês
                 PYG - Guarani Paraguaio   UYU - Peso Uruguaio
                 CHF - Franco Suiço        CLP - Peso Chileno
+                ARS - Peso Argentino      BOB - Boliviano Boliviano
+                COP - Peso Colombiano     CUP - Peso Cubano     
                 
                 É possível colocar o código de qualquer outra moeda conhecida
                 
                 ******************************************************
                 """;
+
+        try {
 
         System.out.println(menu);
         System.out.println("digite o código da moeda atual: ");
@@ -39,19 +44,26 @@ public class Main {
         var amount = scanner.nextDouble();
 
         CurrencyRequest currencyRequest = new CurrencyRequest();
-        Conversion conversion = currencyRequest.conversion(baseCurrency,targetCurrency, amount);
 
-        if (conversion.base_code() != null || conversion.target_code() != null) {
-            System.out.println("A conversão de " + conversion.base_code() + " " + String.format("%.2f", amount));
-            System.out.println("Resulta em " + conversion.target_code() + " " + String.format("%.2f", conversion.conversion_result()));
-            System.out.println("A taxa de conversão verificada foi de " + conversion.conversion_rate());
 
-            System.out.println("Obrigado por utilizar nossos serviços");
 
-        } else {
-            System.out.println("Digitou algum codigo de moeda errado, favor corrigir");
+
+            Conversion conversion = currencyRequest.conversion(baseCurrency, targetCurrency, amount);
+
+
+            if (conversion.base_code() != null || conversion.target_code() != null) {
+                System.out.println("A conversão de " + conversion.base_code() + " " + String.format("%.2f", amount));
+                System.out.println("Resulta em " + conversion.target_code() + " " + String.format("%.2f", conversion.conversion_result()));
+                System.out.println("A taxa de conversão verificada foi de " + conversion.conversion_rate());
+
+                System.out.println("Obrigado por utilizar nossos serviços");
+
+            } else {
+                System.out.println("Digitou algum codigo de moeda errado, favor corrigir");
+            }
+        }catch (InputMismatchException e){
+            System.out.println("Erro de digitação, comece denovo");
         }
-
 
     }
 
